ea53f731be8625eca2b6a1c6d7eb9a2e0d90aed6
Pietro <pietro.butti.fl@gmail.com>
2021-06-18 18:37:20 +0200

    Qualche modifica

--- TEK_creutz/dumb.py
+++ TEK_creutz/dumb.py
@@ -8,41 +8,42 @@ import gvar as gv
 
 from tools import Jackknife, Rebin_vector, std_dev
 
+cf = 0.05
+def chin(x,a,b,c):
+    return a*(   1.-np.exp( -b/(x+c) )   )
 
-def chin(nsm,a,b,c):
-    return a*(1.-np.exp(-b/(nsm+c)))
-
-
-def chir(r,a,b):
-    cf = 0.05
-    return a*(1.-np.exp(-b/(6.*cf/8.*(r*r))))
 
 ciao = np.loadtxt('DATA/PROCESSED/n361b0340k7hf1850_creutz.dat').T
 
 
-rsm = ciao[1]
+rsm = np.array(ciao[1])
 for R in [int(sys.argv[1])]:
     chi = ciao[2*R]
     chi_err = ciao[2*R+1]
 
-    minfit = int(sys.argv[2])
-    maxfit = int(sys.argv[3])
+    if len(sys.argv)==4:
+        minfit = int(sys.argv[2])
+        maxfit = int(sys.argv[3])
 
-    x = ciao[0][minfit:maxfit]
-    y = np.array(ciao[2*R][minfit:maxfit])
-    erry = np.array(ciao[2*R+1][minfit:maxfit])
-
-    yy = gv.gvar(y,erry)
-    # yy = 1/gv.log(1-yy/0.1668)
+        x = ciao[0][minfit:maxfit]
+        y = np.array(ciao[2*R][minfit:maxfit])
+        erry = np.array(ciao[2*R+1][minfit:maxfit])
 
 
-    fit = curve_fit(chin,x,y,sigma=erry,p0=[1.,1.,0.])[0]
-    print(fit)
+        fit,errfit = curve_fit(chin,x,y,sigma=erry,p0=[0.1668,40,0.1])
+        print(minfit,maxfit,gv.gvar(fit,np.diag(errfit)))
+        xfit = np.arange(.01,x[-1],.01)
+        yfit = chin(xfit,*fit)
+        plt.plot(xfit,yfit,label=str(gv.gvar(fit,np.diag(errfit))))
+        plt.errorbar(ciao[0][:maxfit],ciao[2*R][:maxfit],yerr=ciao[2*R+1][:maxfit],fmt='.',label='$\chi$('+str(R)+'.5)')
+        plt.axvline(minfit,color='gray',alpha=0.7)
+        plt.axvline(maxfit,color='gray',alpha=0.7)
 
+        plt.grid(zorder=0, linewidth=0.5, color='gainsboro')
+        plt.legend()
+        plt.savefig('PLOTS/'+'chi('+str(R)+',5)')
+        plt.show()
 
-    xfit = np.arange(.01,x[-1],.01)
-    yfit = chin(xfit,*fit)
-    plt.errorbar(x,[i.mean for i in yy],yerr=[i.sdev for i in yy],fmt='.')
-    plt.plot(xfit,yfit)
-
-    plt.show()
+    else:
+        plt.errorbar(ciao[0],ciao[2*R],yerr=ciao[2*R+1],fmt='.',label='$\chi$('+str(R)+'.5)')
+        plt.show()
