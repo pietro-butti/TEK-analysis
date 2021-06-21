f32f13e294b67ce4ce3fe4a2a512597720dbdeda
Pietro <pietro.butti.fl@gmail.com>
2021-06-21 16:25:43 +0200

    Sto riscrivendo la classe, harvest da la possibilita' di salvare su un file

--- TEK_flow/ciao.sh
+++ TEK_flow/ciao.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-for kappa in 1775 1800 1825 #1875
+for kappa in 1875 #1800 1825 #1875
 do
-    folder=HYDRA/nfh/n169b0350k5hf$kappa
+    folder=/home/pietro/Desktop/DATA_LOCAL/TEK_flow/nfh/n289b0350k5hf$kappa
+    # folder=HYDRA/nfh/n169b0350k5hf$kappa
+    
 
-
-    for file in $(ls $folder/*)
+    for file in $(ls $folder)
     do
-        jobn=$(echo $file | awk -F'_' '{print $2}')
+        jobn=$(echo $file | awk -F'_' '{print $2}' | sed 's/^0*//')
         if [ "$jobn" -lt "10" ]; then
-            mv $file $folder/job_00$jobn
+        #     mv $file $folder/job_00$jobn
+            mv $folder/$file $folder/job_00$jobn
         elif [ "$jobn" -lt "100" ]; then
-            mv $file $folder/job_0$jobn
+            mv $folder/$file $folder/job_0$jobn
         fi
     done
 
